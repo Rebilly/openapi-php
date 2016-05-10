6dc770d3702b6491bb8c697804f05d821daa5d2a
Veaceslav Medvedev <slavcopost@gmail.com>
2016-05-11 00:23:12 +0300

    Configure PhpUnit tests

---
+++ tests/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Slavcodev Components.
+ *
+ * @author Veaceslav Medvedev <slavcopost@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+
+if (!(@require dirname(__DIR__) . '/vendor/autoload.php')) {
+    exit('You must set up the project dependencies, run composer install command');
+}
