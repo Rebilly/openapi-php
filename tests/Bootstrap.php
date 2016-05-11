3dd57874d19969df17d7924e56b63bb96e90e612
Veaceslav Medvedev <slavcopost@gmail.com>
2016-05-12 02:20:41 +0300

    Update files signature

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Slavcodev Components.
+ * This file is part of Rebilly.
  *
- * @author Veaceslav Medvedev <slavcopost@gmail.com>
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://rebilly.com
  */
 
 if (!(@require dirname(__DIR__) . '/vendor/autoload.php')) {
