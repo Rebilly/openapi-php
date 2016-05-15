1d5edb3d7c5d7d5b17a14c71e204db005a654a74
Veaceslav Medvedev <slavcopost@gmail.com>
2016-05-15 04:20:24 +0300

    Implemented  that asserts that the headers list matches schema (#9)

---
+++ src/PhpUnit/HeadersConstraint.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * This file is part of Rebilly.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://rebilly.com
+ */
+
+namespace Rebilly\OpenAPI\PhpUnit;
+
+use PHPUnit_Framework_Constraint as Constraint;
+use Rebilly\OpenAPI\JsonSchema\Validator;
+use Rebilly\OpenAPI\UnexpectedValueException;
+
+/**
+ * Constraint that asserts that the headers list matches the expected defined schemas.
+ */
+final class HeadersConstraint extends Constraint
+{
+    /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
+     * @var array
+     */
+    private $expectedHeadersSchemas;
+
+    /**
+     * @var Validator
+     */
+    private $validator;
+
+    /**
+     * @param array $expectedHeadersSchemas
+     */
+    public function __construct(array $expectedHeadersSchemas) {
+        parent::__construct();
+
+        $this->expectedHeadersSchemas = array_map([$this, 'normalizeJsonSchema'], $expectedHeadersSchemas);
+        $this->validator = new Validator('undefined');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function matches($actualHeaders)
+    {
+        if (!is_array($actualHeaders)) {
+            throw new UnexpectedValueException('Array expected');
+        }
+
+        foreach ($this->expectedHeadersSchemas as $name => $expectedSchema) {
+            if (isset($actualHeaders[$name])) {
+                $errors = $this->validator->validate(
+                    $this->normalizeHeaderValue($actualHeaders[$name]),
+                    $expectedSchema,
+                    $name
+                );
+                $this->errors = array_merge($this->errors, $errors);
+            } elseif (isset($expectedSchema->required) && $expectedSchema->required) {
+                $this->errors[] = [
+                    'property' => $name,
+                    'message' => "Missing required header ({$name})",
+                ];
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function failureDescription($other)
+    {
+        return json_encode($other) . ' ' . $this->toString();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function additionalFailureDescription($other)
+    {
+        return $this->validator->serializeErrors($this->errors);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toString()
+    {
+        return 'matches an specified headers schemas';
+    }
+
+    /**
+     * Ensure schema is object.
+     *
+     * @param object|array $schema
+     *
+     * @return object
+     */
+    private static function normalizeJsonSchema($schema)
+    {
+        return (object) $schema;
+    }
+
+    /**
+     * Ensure header value is array.
+     *
+     * @param mixed $value
+     *
+     * @return array
+     */
+    private static function normalizeHeaderValue($value)
+    {
+        if (is_string($value)) {
+            return preg_split('#\s*,\s*#', $value, -1, PREG_SPLIT_NO_EMPTY);
+        } else {
+            return (array) $value;
+        }
+    }
+}
---
+++ tests/PhpUnit/HeadersConstraintTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * This file is part of Rebilly.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://rebilly.com
+ */
+
+namespace Rebilly\OpenAPI\PhpUnit;
+
+use Exception;
+use Rebilly\OpenAPI\TestCase;
+use Rebilly\OpenAPI\UnexpectedValueException;
+
+/**
+ * Class HeadersConstraintTest.
+ */
+class HeadersConstraintTest extends TestCase
+{
+    /**
+     * @var HeadersConstraint
+     */
+    private $constraint;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->constraint = new HeadersConstraint(
+            [
+                'Content-Type' => [
+                    'type' => 'array',
+                    'required' => true,
+                    'minItems' => 1,
+                    'items' => [
+                        'type' => 'string',
+                    ],
+                ],
+                'Allow' => [
+                    'type' => 'array',
+                    'minItems' => 2,
+                    'items' => [
+                        'type' => 'string',
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function passUnexpectedValue()
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('Array expected');
+
+        $this->assertThat((object) [], $this->constraint);
+    }
+
+    /**
+     * @test
+     */
+    public function AssertValidHeaders()
+    {
+        $headers = ['Content-Type' => ['application/json'], 'Allow' => 'OPTIONS, HEAD'];
+
+        $this->assertThat($headers, $this->constraint);
+    }
+
+    /**
+     * @test
+     * @dataProvider provideInvalidHeaders
+     *
+     * @param array $headers
+     */
+    public function assertInvalidHeaders(array $headers)
+    {
+        $error = $this->getDataSetName();
+
+        try {
+            $this->assertThat($headers, $this->constraint);
+        } catch (Exception $e) {
+            $this->assertContains(
+                $error,
+                $e->getMessage(),
+                "Failed asserting that passed invalid URI: {$error}"
+            );
+        }
+
+        if (!isset($e)) {
+            $this->fail('Failed asserting that passed invalid URI');
+        }
+    }
+
+    public function provideInvalidHeaders()
+    {
+        return [
+            'Missing required header' => [
+                [],
+            ],
+            '[Allow] There must be a minimum of 2 items in the array' => [
+                [
+                    'Content-Type' => 'application/json',
+                    'Allow' => 'OPTIONS',
+                ],
+            ],
+        ];
+    }
+}
